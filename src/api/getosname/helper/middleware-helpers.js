0c2e8b2c7b7dd73594e04f80ffff77bba4d84d16
KarthickMuthukrishnan-cog <karthick.muthukrishnan@cognizant.com>
2026-05-19 16:26:12 +0100

    sonar fix

--- src/api/getosname/helper/middleware-helpers.js
+++ src/api/getosname/helper/middleware-helpers.js
@@ -66,9 +66,9 @@ const processMatches = (matches, locationNameOrPostcode, userLocation) => {
 }
 
 const convertStringToHyphenatedLowercaseWords = (input) => {
-  const removedHyphens = input.replaceAll(/ - /g, ' ')
+  const removedHyphens = input.replaceAll(' - ', ' ')
   // Remove commas, convert to lowercase, and split the string into words
-  const words = removedHyphens.replaceAll(/,/g, '').toLowerCase().split(' ')
+  const words = removedHyphens.replaceAll(',', '').toLowerCase().split(' ')
 
   // Join the words with hyphens
   return words.join('-')
